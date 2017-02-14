0157f4e4a0921c9767b4f8c261235c4e7eae8613
SheilaRos <sheila.ros.cheme@gmail.com>
2017-02-14 20:05:06 +0100

    initial commit

--- LeagueOfStats/ViewController.swift
+++ LeagueOfStats/ViewController.swift
@@ -3,13 +3,13 @@
 //  LeagueOfStats
 //
 //  Created by DAM on 14/2/17.
-//  Copyright © 2017 Sheila. All rights reserved.
+//  Copyright © 2017 Sheila & Bilal. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    //no me deja hacer commits...asi no se puede...
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
